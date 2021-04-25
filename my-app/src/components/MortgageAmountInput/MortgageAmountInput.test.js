9265943efb12b532db2132b9f23b64eab7258caf
Mae <maeberris@outlook.com>
2021-04-25 18:07:51 -0400

    Add MortgageInput test, work on other form inputs

--- my-app/src/components/MortgageAmountInput/MortgageAmountInput.test.js
+++ my-app/src/components/MortgageAmountInput/MortgageAmountInput.test.js
@@ -1,10 +1,20 @@
-import { screen, render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import { screen, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import MortgageAmountInput from "./MortgageAmountInput";
+import Form from "../Form/Form";
 
 describe("MortgageAmountInput", () => {
   test("renders MortgageAmountInput", () => {
     render(<MortgageAmountInput />);
     expect(screen.getByText("Mortgage Amount")).toBeInTheDocument();
+  });
+  test("changes value of MortgageInput on user input", async () => {
+    render(<Form />);
+    const mortgageInput = screen.getByRole("mortgageAmount");
+
+    fireEvent.change(mortgageInput, { target: { value: "1555" } });
+
+    expect(screen.getByRole("mortgageAmount").value).toBe("1,555");
   });
 });
